6e1860a03630dcb38890734252513ec0987acf85
Naomi-Rhames <naomirhames2020@gmail.com>
2021-11-12 23:23:31 -0600

    adding to login funtionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::API
     def encode_token(user_id)
         JWT.encode({user_id: user_id}, ENV["JWT_SECRET"])
     end
+
+
+    private
+
+    def current_user_token
+        request.headers['Authorization']
+    end
+
+    def decode_current_user_token
+        JWT.decode(current_user_token, ENV["JWT_SECRET"])
+    end
 end
