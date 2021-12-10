0d54098de0cc2080ebd9d03a0d37acd7b92ccc7f
Naomi-Rhames <naomirhames2020@gmail.com>
2021-12-10 16:07:51 -0600

    added username

--- app/serializers/tweet_serializer.rb
+++ app/serializers/tweet_serializer.rb
@@ -1,7 +1,10 @@
 class TweetSerializer < ActiveModel::Serializer
-attributes :id, :description, :image_url
+attributes :id, :description, :image_url, :username
 has_many :comments
 
+def username
+    self.object.user.username
+  end
 
    
 end 
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :username, :email, :bio, :id
+
+
+
 end
